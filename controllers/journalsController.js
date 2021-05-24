9fd0430144bf01ae92df42ab6abaa2b560215899
carmela12345678 <18101172@usc.edu.ph>
2021-05-24 17:20:01 +0800

    change pass, date column ako ge remove

--- controllers/journalsController.js
+++ controllers/journalsController.js
@@ -53,7 +53,9 @@ let add = (req,res) => {
                 res.render('journals/add', {
                     user_id: form_data.user_id,
                     title: form_data.title,
-                    description: form_data.description                    
+                    description: form_data.descriptions,
+                    created_at: form_data.created_at,
+                    updated_at: form_data.updated_at                     
                 })
             } else if(result) {                
                 req.flash('success', 'Journal successfully added');
