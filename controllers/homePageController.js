20b3d54b474c83d0800abcb35defb315ba39e7b0
Blissful18 <nicole_mhelly@yahoo.com>
2021-05-26 17:22:09 +0800

    HOMEPAGE WERKING

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -3,7 +3,16 @@ const { NOW } = require("sequelize");
 const connection = require("../configs/DBConnection");
 
 
-let handleHelloWorld = (req, res) => res.render("home/home",{user_id:req.user.user_id});
+let handleHelloWorld = (req, res) => {
+	let userId = req.user.user_id;
+    connection.query('SELECT * FROM journal_posts WHERE user_id =' + userId, function(err,rows)     {
+        if(!err) {
+            // render to views/journals/journals.ejs
+            res.render('home/home',{data: rows});
+        }
+    });
+	// res.render("home/home",{user_id:req.user.user_id});
+}
 
 let getFeed = (req, res) => {
 	homePageService
@@ -15,6 +24,16 @@ let getFeed = (req, res) => {
 			res.status(500).send();
 		});
 };
+
+// let index = async (req,res) => {
+//     let userId = req.user.user_id;
+//     connection.query('SELECT * FROM journal_posts WHERE user_id =' + userId, function(err,rows)     {
+//         if(!err) {
+//             // render to views/journals/journals.ejs
+//             res.render('home/home',{data: rows});
+//         }
+//     });
+// }
 
 module.exports = {
 	handleHelloWorld,
